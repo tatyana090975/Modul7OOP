e1a57d7ad94126014beb15d1b47b4d2036475c57
татьяна090975 <tatyana090975@gmail.com>
2024-01-21 21:09:07 +0400

    Задание 7.1.4

--- Program.cs
+++ Program.cs
@@ -8,6 +8,23 @@ namespace Modul7OOP
 {
     internal class Program
     {
+        class Employee
+        {
+            public string Name;
+            public int Age;
+            public int Salary;
+        }
+
+        class ProjectManager: Employee
+        {
+            public string ProjectName;
+        }
+
+        class Developer: Employee
+        {
+            private string ProgrammingLanguage;
+        }
+
         static void Main(string[] args)
         {
         }
