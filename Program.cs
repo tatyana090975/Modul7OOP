36380adfa177711c12faa01cbd298a6d618f3c04
татьяна090975 <tatyana090975@gmail.com>
2024-01-22 17:20:17 +0400

    Задание 7.1.7

--- Program.cs
+++ Program.cs
@@ -6,26 +6,45 @@ using System.Threading.Tasks;
 
 namespace Modul7OOP
 {
-    internal class Program
+    class Obj
     {
-        class Obj
-        {
-            private string name;
-            private string owner;
-            private int length;
-            private int count;
+        private string name;
+        private string owner;
+        private int length;
+        private int count;
 
-            public Obj(string name, string ownerName, int objLength, int count)
-            {
-                this.name = name;
-                owner = ownerName;
-                length = objLength;
-                this.count = count;
-            }
+        public Obj(string name, string ownerName, int objLength, int count)
+        {
+            this.name = name;
+            owner = ownerName;
+            length = objLength;
+            this.count = count;
+        }
+    }
+
+    class SmartHelper
+    {
+        private string name;
+
+        public SmartHelper(string name)
+        {
+            this.name = name;
         }
 
+        public void Greetings(string name)
+        {
+            Console.WriteLine("Привет, {0}, я интеллектуальный помощник {1}", name, this.name);
+        }
+    }
+    class Program
+    {
         static void Main(string[] args)
         {
+            SmartHelper helper = new SmartHelper("Олег");
+            helper.Greetings("Грег");
+
+            Console.ReadKey();
         }
+
     }
 }
