88c71b154fb2b9a4077c9293eaff5f9a3dbea699
татьяна090975 <tatyana090975@gmail.com>
2024-01-22 20:13:40 +0400

    Задание 7.1.10

--- Program.cs
+++ Program.cs
@@ -6,6 +6,36 @@ using System.Threading.Tasks;
 
 namespace Modul7OOP
 {
+    class BaseClass
+    {
+        protected string Name;
+
+        public BaseClass(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public string Description;
+
+        public int Counter;
+
+        public DerivedClass(string name, string description): base(name)
+        {
+            Description = description;
+        }
+
+        public DerivedClass(string name, string description, int counter): base(name)
+        {
+            Description = description;
+            Counter = counter;
+        }
+    }
+
+
+
     class Obj
     {
         private string name;
