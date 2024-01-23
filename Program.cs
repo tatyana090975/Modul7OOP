e39e9158a19f5908cb250bfd16df257108b39328
татьяна090975 <tatyana090975@gmail.com>
2024-01-23 21:44:40 +0400

    Задание 7.3.3

--- Program.cs
+++ Program.cs
@@ -7,40 +7,40 @@ using System.Threading.Tasks;
 
 namespace Modul7OOP
 {
-    class Vector
+    abstract class ComputerPart
     {
-        public int X;
-        public int Y;
+        public abstract void Work();
+    }
 
-        public static Vector operator +(Vector a, Vector b)
+    class Processor : ComputerPart
+    {
+        public override void Work()
         {
-            return new Vector
-            {
-                X = a.X + b.X,
-                Y = a.Y + b.Y
-            };
+            
         }
-        /*
-        public Vector Add(Vector second)
+    }
+
+    class MotherBoard : ComputerPart
+    {
+        public override void Work()
         {
-            return new Vector
-            {
-                X = this.X + second.X,
-                Y = this.Y + second.Y
-            };
-        }*/
+            
+        }
+    }
+
+    class GraphicCard : ComputerPart
+    {
+        public override void Work()
+        {
+            
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Vector a = new Vector { X = 3, Y = 5 };
-            Vector b = new Vector { X = 7, Y = 13 };
-
-            Vector c = a + b;
-
-            Console.WriteLine($"{c.X}, {c.Y}");
+            
 
             Console.ReadKey();
         }
