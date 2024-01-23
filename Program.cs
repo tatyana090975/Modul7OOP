07d7769beb1fdc53e99420c107ea56f9f602fb5b
татьяна090975 <tatyana090975@gmail.com>
2024-01-23 14:28:38 +0400

    Задание 7.2.5

--- Program.cs
+++ Program.cs
@@ -9,19 +9,6 @@ namespace Modul7OOP
 {
     class BaseClass
     {
-        protected string Name;
-
-        public virtual int Counter
-        {
-            get;
-            set;
-        }
-
-        public BaseClass(string name)
-        {
-            Name = name;
-        }
-
         public virtual void Display()
         {
             Console.WriteLine("Метод класса BaseClass");
@@ -30,80 +17,19 @@ namespace Modul7OOP
 
     class DerivedClass : BaseClass
     {
-        public string Description;
-
-        //private int Counter;
-
-        public override int Counter 
-        {
-            get
-            {
-                return Counter;
-            }
-            set
-            {
-                if (Counter >= 0)
-                {
-                    Counter = value;
-                }
-            }      
-        }
-
-        public DerivedClass(string name, string description): base(name)
-        {
-            Description = description;
-        }
-
-        public DerivedClass(string name, string description, int counter): base(name)
-        {
-            Description = description;
-            Counter = counter;
-        }
-
         public override void Display()
         {
+            base.Display();
             Console.WriteLine("Метод класса DerivedClass");
         }
     }
 
-
-
-    class Obj
-    {
-        private string name;
-        private string owner;
-        private int length;
-        private int count;
-
-        public Obj(string name, string ownerName, int objLength, int count)
-        {
-            this.name = name;
-            owner = ownerName;
-            length = objLength;
-            this.count = count;
-        }
-    }
-
-    class SmartHelper
-    {
-        private string name;
-
-        public SmartHelper(string name)
-        {
-            this.name = name;
-        }
-
-        public void Greetings(string name)
-        {
-            Console.WriteLine("Привет, {0}, я интеллектуальный помощник {1}", name, this.name);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            SmartHelper helper = new SmartHelper("Олег");
-            helper.Greetings("Грег");
+            DerivedClass obj = new DerivedClass();
+            obj.Display();
 
             Console.ReadKey();
         }
