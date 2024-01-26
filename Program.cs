3e10b243367daf4bf84fa4bf1c4f577948632ee5
татьяна090975 <tatyana090975@gmail.com>
2024-01-26 10:45:58 +0400

    Задание 7.5.9

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,13 +23,38 @@ namespace Modul7OOP
             int MaxValue = 2000;
         }            
     }
-        
-    
+
+    static class IntExtensions
+    {
+        public static int GetNegative(this int value)
+        {
+            if (value > 0) { return -value; }
+            else return value;
+        }
+
+        public static int GetPositive(this int value)
+        {
+            if (value < 0) { return -value; }
+            else return value;
+        }
+    }    
+
     class Program
     {
+        
         static void Main(string[] args)
         {
-            
+            int num1 = 7;
+            int num2 = -13;
+            int num3 = 0;
+
+            Console.WriteLine(num1.GetNegative()); //-7
+            Console.WriteLine(num1.GetPositive()); //7
+            Console.WriteLine(num2.GetNegative()); //-13
+            Console.WriteLine(num2.GetPositive()); //13
+            Console.WriteLine(num3.GetNegative()); //0
+            Console.WriteLine(num3.GetPositive());
+
             Console.ReadKey();
         }
 
